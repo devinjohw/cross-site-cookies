bc283e2b64c0f3d932dcf1442dba065f3b3c88fe
Devin <devin.johw@gmail.com>
2024-12-16 00:49:04 +0800

    add ecosystem

--- index.ts
+++ index.ts
@@ -1,30 +1,34 @@
 import express from 'express'
 import type { Request, Response } from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import path from 'path'
+// import cors from 'cors'
+// import cookieParser from 'cookie-parser'
 
 const app = express()
 
-app.use(cookieParser())
+// app.use(cookieParser())
 
-app.use(cors({
-  origin: ['http://127.0.0.1:3001', 'http://localhost:3001'],
-  credentials: true
-}))
+// app.use(cors({
+//   origin: ['http://127.0.0.1:3001', 'http://localhost:3001'],
+//   credentials: true
+// }))
 
-app.post('/set-cookie', (req: Request, res: Response) => {
-  res.cookie('name', 'devin', {
-    maxAge: 1000 * 60 * 60 * 24 * 30,
-    httpOnly: true,
-    sameSite: 'none',
-    secure: true
-  })
-  res.status(204).send();
-})
+// app.post('/set-cookie', (req: Request, res: Response) => {
+//   res.cookie('name', 'devin', {
+//     maxAge: 1000 * 60 * 60 * 24 * 30,
+//     httpOnly: true,
+//     sameSite: 'none',
+//     secure: true
+//   })
+//   res.status(204).send();
+// })
 
-app.get('/upload-cookie', (req: Request, res: Response) => {
-  console.log(req.cookies)
-  res.json({ cookies: req.cookies })
+// app.get('/upload-cookie', (req: Request, res: Response) => {
+//   console.log(req.cookies)
+//   res.json({ cookies: req.cookies })
+// })
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.resolve('pages/index.html'))
 })
 
 
